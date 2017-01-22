209833fe4f2b16fe1a05c172f4260bcd5a6ee6ca
AngePang <pkuann@gmail.com>
2017-01-22 15:02:02 -0500

    fill makeCacheMatrix
    Realize the mCM function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(inverse)    inv <<- inverse
+  getinv <- function()  inv
+  return(list(set = set, get = get,
+              setinv = setinv,
+              getinv = getinv))
 }
 
 
